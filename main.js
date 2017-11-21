499b804d2100af74f59ee99c79c6bdc98442478c
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-11-21 15:34:39 -0500

    score counter

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 var cookie = document.getElementById('cookie')
 
-var score = document.getElementsByClassName('.score')
+var score = document.getElementsByClassName('score')
 
-var pointsShown = 0
-
-var score = 0
-
+var points = 0
 
 // FUNCTION: WHEN CLICKED, INCREASE SCORE:
 
 var upScore = function() {
-
-    for (var i = 0; i >= 0; i++) {
-        score += 10;
-        pointsShown.innerHTML = score;
-    }
+        points += 10;
+        score[0].innerHTML = points;
 }
+
+// RESET SCORE 
+
+var reset = document.getElementById('reset')
+var zeroOut = function() {
+        points = 0;
+        score[0].innerHTML = points
+}
+
+reset.addEventListener('click', zeroOut)
+
 
 // listen for cookie click/ do thing
 
-cookie.addEventListener('click', upScore());
+cookie.addEventListener("click", upScore);
 
+// add cookie monster
+
+if (score >= 100 && score <= 130) {
+    
+}
